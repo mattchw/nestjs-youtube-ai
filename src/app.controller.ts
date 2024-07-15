63002e354dfe3e18d8596b9f15195f545d732a37
Matthew Wong <matthew.chohin@gmail.com>
2024-07-15 14:49:17 +0100

    fix: remove unnecessary functions

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getStatus(@Res() res: Response) {
+    return res.status(HttpStatus.OK).json({ message: "status check - OK" });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/config.module';
 import { YoutubeModule } from './youtube/youtube.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
     AppConfigModule,
     YoutubeModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule { }
